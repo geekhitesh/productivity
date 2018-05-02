693e20d216e07e8d239c77ec2378a01e720522f0
Hitesh Ahuja <hiteshah@amdocs.com>
2018-05-02 15:39:58 +0530

    updated comments

--- app/Order.php
+++ app/Order.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     //
-
+    // Added changes to this file.
     public function product_instances()
     {
     	return $this->hasMany('App\ProductInstance');
