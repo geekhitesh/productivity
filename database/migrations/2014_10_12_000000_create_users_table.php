3b136e1f4107f5f84ece6df1d4d80c5f4791bd1d
Hitesh Ahuja <hiteshah@amdocs.com>
2018-05-03 22:26:40 +0530

    updated migrations for users and customers

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('first_name');
             $table->string('last_name');
-            $table->integer('mobile_no');
+            $table->string('mobile_no',30);
             $table->string('profile_photo_url');
             $table->date('date_of_birth');
             $table->string('email')->unique();
